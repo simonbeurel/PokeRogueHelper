124b20d89fc86d35691078fcfff2b772fe0dbad0
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2024-06-06 04:47:51 +0200

    Retrieve/Sort/Print the resistances about a pokemon

--- popup.js
+++ popup.js
@@ -1,3 +1,20 @@
+function retrieve_sorted_resistances(data) {
+    let arrayPokemonResistances = {};
+    data.resistances.forEach(resistance => {
+        arrayPokemonResistances[resistance.name] = resistance.multiplier;
+    });
+    //sort the resistances
+    var keys = Object.keys(arrayPokemonResistances);
+    keys.sort(function (a, b) {
+        return arrayPokemonResistances[a] - arrayPokemonResistances[b];
+    });
+    var sortedResistances = {};
+    keys.forEach(function (key) {
+        sortedResistances[key] = arrayPokemonResistances[key];
+    });
+    return sortedResistances;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 
     var captureButton = document.getElementById('captureButton');
@@ -7,8 +24,8 @@ document.addEventListener('DOMContentLoaded', function() {
             image.onload = function() {
                 var canvas = document.createElement('canvas');
                 var context = canvas.getContext('2d');
-                canvas.width = image.width / 2; // La moitié de la largeur de l'image
-                canvas.height = image.height / 2; // La moitié de la hauteur de l'image
+                canvas.width = image.width / 2; // Half of the image width
+                canvas.height = image.height / 2; // Half of the image height
                 context.drawImage(image, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
                 var croppedDataUrl = canvas.toDataURL('image/png');
                 var croppedImage = document.createElement('img');
@@ -17,6 +34,34 @@ document.addEventListener('DOMContentLoaded', function() {
             };
             image.src = dataUrl;
         });
+    });
+
+    var searchPokemon = document.getElementById('searchPokemon');
+    searchPokemon.addEventListener('click', function() {
+        var input = document.getElementById('pokemonInput');
+        var pokemonName = input.value.toLowerCase(); // Convert to lowercase
+        var url = 'https://tyradex.vercel.app/api/v1/pokemon/' + pokemonName;
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Pokemon not found');
+                }
+                return response.json();
+            })
+            .then(data => {
+                var sortedResistances = retrieve_sorted_resistances(data);
+                // Display resistances
+                var resistances = document.getElementById('result');
+                resistances.innerHTML = '';
+                for (var key in sortedResistances) {
+                    var resistance = document.createElement('li');
+                    resistance.textContent = key + ': ' + sortedResistances[key];
+                    resistances.appendChild(resistance);
+                }
+            })
+            .catch(error => {
+                alert(error.message); // Display error message
+            });
     });
 
 });
--- retrieve_text.js
+++ retrieve_text.js
@@ -1,10 +1,13 @@
-const Tesseract = require('tesseract.js');
+const apiKey = "K84556445788957";
+const imageUrl = "https://dl.a9t9.com/ocr/solarcell.jpg";
+const apiUrl = `https://api.ocr.space/parse/imageurl?apikey=${apiKey}&url=${encodeURIComponent(imageUrl)}`;
 
-Tesseract.recognize(
-    './image2.png',
-    'fra'
-).then(({ data: { text } }) => {
-    console.log(text);
-}).catch(error => {
-    console.error(error);
-});
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Faire quelque chose avec les données retournées
+        console.log(data.ParsedResults[0].ParsedText);
+    })
+    .catch(error => {
+        console.error('Une erreur s\'est produite:', error);
+    });
